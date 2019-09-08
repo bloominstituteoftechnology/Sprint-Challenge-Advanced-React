6f1f4f9870438927a9d6050abe477dac8b46cae5
Bangstry <tylerwesleyquinn@gmail.com>
2019-09-07 23:47:22 -0400

    test are fun....

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,3 +3,22 @@ import ReactDOM from 'react-dom';
 import App from './App';import "@testing-library/jest-dom/extend-expect";
 import * as rtl from "@testing-library/react";
 
+test('class changes on darkmode button',()=>{    
+
+  const app = rtl.render(<App />);
+  const dark = app.getByText(/dark mode/i);
+  app.unmount()
+  
+
+})
+
+// test('no crash on render', async () => {
+//   const DOM = await rtl.render(<App />);
+
+//   console.log(DOM);
+// });
+
+
+// test('i am not crazy amanda', ()=>{
+//   expect(true).toBe(true)
+// })
