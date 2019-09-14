a27534b4f4d32e790f8f3415507123f6ba14bb2b
Kayla Dailey <kayladailey000@gmail.com>
2019-09-13 21:40:58 -0500

    Restarted project. Data pulled in console.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component}from 'react';
+import { Card } from 'semantic-ui-react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      player: []
+    };
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:5000/api/players")
+      .then(res => res.json())
+      .then(res => console.log(res) )
+      .then(res => {
+        //debugger;
+        this.setState({player:res})})
+      .catch(err => console.log('Error', err))
+  }
+
+  render() {
+    return(
+  
+    <div className='Profile'>
+      <Card className='Profile-Card'>
+        <Card.Content>
+          <Card.Header>{this.state.player.name} </Card.Header>
+            {this.state.player.contry}
+        </Card.Content>
+      </Card>
     </div>
-  );
-}
+    )}}
 
 export default App;
