e038aa7b498a3da58620104bf1694464ea566dd0
Dominick Bruno <dbruno@unseen.is>
2019-09-13 22:59:01 -0400

    Successfully fetched and rendered data with Class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API = 'http://localhost:5000/api/players';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: [],
+    };
+  }
+  componentDidMount() {
+    Axios.get(API)
+     .then(res => {
+      console.log(res.data)
+      this.setState({players: res.data})
+    })
+  }
+  render() {
+    const { players } = this.state;
+    console.log(players);
+      return (
+      <ul>
+        
+        {this.state.players.map(player =>
+          <li key={player.id}>
+        {player.name} from {player.country} with {player.searches} searches.
+          </li>
+        )}
+      </ul>
+    );
+  }
 }
-
 export default App;
