3284036d3fad10679a3a01b19be94f882c908b9f
triggering-event <gabriel.j.shapiro@gmail.com>
2019-09-16 00:04:54 -0400

    fixes counter issue

--- client/src/DisplayPlayers.js
+++ client/src/DisplayPlayers.js
@@ -1,26 +1,23 @@
 import React, {useState} from 'react';
 import {useInput} from "./playerLiker";
 
+export const increaseStat = value => {
+    return value + 1;
+}
+
 const DisplayPlayers = (props) => {
 
     const [playerLikes, setPlayerLikes, handlePlayerLikes] = useInput("likes", 0);
 
-    const clickHandler = (numString) => {
-        let num = parseInt(numString);
-        let newNum = num++;
-        setPlayerLikes(newNum.toString());
-    }
-
     return (
         <div className="playerBoxesContainer">
-            <h1>Number of Liked Players: {}</h1>
+            <h1>Number of Liked Players: {playerLikes}</h1>
             {props.players.map((player) =>
                 <div key={player.id} className="playerBox">
                     <h1>{player.name}</h1>
                     <h2>Country: {player.country}</h2>
                     <h2># of Searches: {player.searches}</h2>
-                    <h2># of Likes: {playerLikes}</h2>
-                    <button onClick={(playerLikes) => clickHandler(playerLikes)}>Like</button>
+                    <button onClick={() => setPlayerLikes(increaseStat(playerLikes))}>Like</button>
                 </div>
             )}
         </div>
