e2ced783b5d36118647f4bb1b94668f2a246650e
Brian <lawmen218@yahoo.com>
2019-09-16 23:09:43 -0400

    completed everything except need more testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import PlayerList from "./Components/PlayerList";
+import NavBar from "./Components/NavBar";
 
 
 
@@ -15,13 +16,17 @@ class App extends React.Component {
   componentDidMount() {
     fetch('http://localhost:5000/api/players')
       .then(res => res.json())
-      .then(data => this.setState({ player: data }))
+      .then(data => {
+
+        this.setState({ player: data })
+      })
       .catch(err => console.log(err))
   }
 
   render() {
     return (
       <div className="App">
+        <NavBar />
         <PlayerList players={this.state.player} />
       </div>
     );
--- client/src/Components/PlayerCard.js
+++ client/src/Components/PlayerCard.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Card, Icon, Image } from 'semantic-ui-react'
 
 const PlayerCard = ({ key, name, country, searches }) => {
     return (
         <div className="playercard" data-testid={key}>
-            <h2>Name: {name}</h2>
-            <h3>Country: {country}</h3>
-            <h3>Searches: {searches}</h3>
+            <Card>
+                <Card.Header><h2>Name: {name}</h2></Card.Header>
+                <Card.Meta>
+                    <h3>Country: {country}</h3>
+                </Card.Meta>
+                <Card.Description>
+                    <h3>
+                        <Icon name="user" />
+                        Searches: {searches}</h3>
+                </Card.Description>
+            </Card>
+
         </div>
     )
 }
