fddbab33bf85952b83606b77480af61d9b417fda
Brandon Davis <49970067+Bdavis90@users.noreply.github.com>
2019-09-17 00:02:06 -0400

    MVP

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -7,20 +7,24 @@ import PlayerForm from "./components/PlayerForm";
 import { fireEvent } from "@testing-library/react";
 
 it("renders without crashing", () => {
-  rtl.render(<App />);
+  const app = rtl.render(<App />);
+  console.log(app.debug());
 });
 
 it("renders players in app", () => {
   const app = rtl.render(<App />);
   app.getByText(/players/i);
+  console.log(app.debug());
 });
 
 it("renders player input", () => {
   const playerInput = rtl.render(<PlayerForm />);
   playerInput.getByTestId(/player/i);
+  console.log(playerInput.debug());
 });
 
 it("renders country input", () => {
   const playerInput = rtl.render(<PlayerForm />);
   playerInput.getByTestId(/country/i);
+  console.log(playerInput.debug());
 });
--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -3,9 +3,12 @@ import React from "react";
 const Players = props => {
   console.log("players props: ", props);
   return (
-    <div className="player-card" data-testid="players">
-      <h2>Name: {props.player.name}</h2>
-      <h3>Country: {props.player.country}</h3>
+    <div>
+      <div className="player-card" data-testid="players">
+        <h2>Name: {props.player.name}</h2>
+        <h2>Searches: {props.player.searches}</h2>
+        <h3>Country: {props.player.country}</h3>
+      </div>
     </div>
   );
 };
