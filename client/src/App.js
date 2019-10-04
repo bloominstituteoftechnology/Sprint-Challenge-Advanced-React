4da9f0bf4c0844a35d966f5b528be6dbb8dbb860
andrew15722 <andrew15722@yahoo.com>
2019-10-04 08:47:07 -0700

    got my app set up and all the data coming back from the api using a custom hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+import { useAxios } from './Hooks/useAxios.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ data, setData ] = useAxios([]);
+	console.log(data);
+	return <div className="App">{/* <CardList /> */}</div>;
 }
 
 export default App;
+
+// Class version
+
+// class App extends React.Component
