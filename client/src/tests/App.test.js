b974047b650d68004e423cf6fffda6bc5828cbd9
Surfsol <surfsol@protonamil.com>
2019-10-04 16:35:30 -0300

    no change

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -5,6 +5,7 @@ import { render} from '@testing-library/react'
 import {voteClickUSA} from '../components/Navbar'
 import PlayersCard from '../components/PlayersCard';
 
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
@@ -17,7 +18,7 @@ it('renders without crashing', () => {
 });
 
 test(`Increase vote by 1`, () => {
-  expect(voteClickUSA(1)).toBe(2);
+  expect(voteClickUSA(0)).toBe(1);
 })
 
 //cheatsheet queries getByText https://testing-library.com/docs/dom-testing-library/cheatsheet
