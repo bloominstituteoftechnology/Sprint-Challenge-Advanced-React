439b95521983576aed5193b216398d6ab0001d33
Amber Sorensen <ambersorensen@ambers-mbp.lan>
2019-10-04 09:18:40 -0600

    added axios, built out basic component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state
+  }
+
+
+componentDidMount(){};
+
+componentDidUpdate(){};
+
+render(){
+  return(
+    <div className='App'>
+      Hardcoded from App
     </div>
   );
 }
-
+  }
 export default App;
