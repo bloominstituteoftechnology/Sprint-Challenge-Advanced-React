31e6247082f2b9c2f4e004c7966245eb6cd0df1d
Muamer Kukic <kukicako@gmail.com>
2019-10-05 08:28:25 -0700

    class component rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+ constructor(){
+   super();
+   this.state = {
+     myData: {},
+     country: []
+   }
+ }
+ componentDidMount(){
+    axios
+     .get("http://localhost:5000/api/players")
+     .then(res => {
+       console.log(res)
+       this.setState({
+         country: res.data
+       })
+     })
+     .catch(err => console.log(err))
+ }
+ render(){
+   return (
+     <div className="App">
+       <div className="follower">
+         {this.state.country.map(follower => (
+           <div key={follower.id}>
+             <p>{follower.name}</p>
+             <p>{follower.country}</p>
+           </div>
+         ))}
+       </div>
+     </div>
+   );
+ }
 }
-
 export default App;
