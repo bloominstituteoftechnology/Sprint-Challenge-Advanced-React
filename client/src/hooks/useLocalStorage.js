7e739b66867257aea286d4f587ec04d764e8366d
spettigrew <sara.pettigrew5@gmail.com>
2019-10-11 19:30:53 -0600

    2nd custom hook setup in useLocalStorage.js

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -1,5 +1,17 @@
 import { useState } from 'react';
 
+export function useInput(initialValue) {
+    const [value, setValue] = useState(initialValue)
+
+    const customSetter = (newValue) => {
+        console.log("New Value:", newValue)
+        setValue(newValue)
+    }
+
+    return [value, customSetter]
+
+}
+
 export function useLocalStorage(key, initialValue) {
     const [storedValue, setStoredValue] = useState(() => {
         const item = window.localStorage.getItem(key);
