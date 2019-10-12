8b1b4f3316ca11ed6ddb7d875ae7218e3d07c1f4
Cole Gonzales <gcolegonzales003@gmail.com>
2019-10-12 13:29:20 -0500

    Completed MVP except for a testing malfinction I was told to address later on

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Display from './components/Display';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/players')
+      .then((res) => {
+        this.setState({
+          users: res.data
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  render(){
+    return (
+      <Display userData={this.state.users}/>
+    )
+  }
 }
 
 export default App;
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import App from './App.js';
+
+afterEach(rtl.cleanup);
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
+  const div = docment.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('loads dark mode button', () => {
+  const app = ReactDOM.render(<App />);
+  const element = app.getByTitle('dark-mode-btn');
+  expect(element).toBeVisible();
 });
