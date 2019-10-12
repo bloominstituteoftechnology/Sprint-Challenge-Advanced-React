b8aaf0b03acdce722a570d1d2609243743b6a454
unknown <alec.thomas.quinn@gmail.com>
2019-10-11 20:24:39 -0500

    test created but lecture did not go over testing w/ API calls

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+afterEach(rtl.cleanup);
+
+test('Render the App', () => {
+  const wrapper = rtl.render(<App />)
+  wrapper.debug()
+})
