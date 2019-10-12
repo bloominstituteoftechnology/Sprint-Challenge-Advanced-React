851b61183d71b19ff1e335f8189a24a275be0c11
spettigrew <sara.pettigrew5@gmail.com>
2019-10-11 18:02:50 -0600

    app, players, localStorage.js set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
-import React, { useState } from 'react';
-import { useLocalStorage } from 
+import React from 'react';
+import { useLocalStorage } from './hooks/localStorage';
 
-import './App.css';
 
-function App() {
+// import './App.css';
+
+function App(props) {
+  const[player, setPlayer] = useLocalStorage('player', 'country');
+  const[search, setSearch] = useLocalStorage('search', 1);
+  // const [players] = useState([]);
+
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <h1>Women's World Cup - 2019 </h1>
+      <select value={player} onChange={e => setPlayer(e.target.value)}> 
+      </select>
+
+      <input
+      type='number' placeholder='Search Count' value={search} onChange={e => setSearch(e.target.value)} />
+
+      {/* <div>
+        {players.map((player, index) => (
+          <
+        ))}
+      </div> */}
   );
 }
 
--- client/src/components/api.js
+++ client/src/components/api.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export function usePlayers(player, country, search-interest){
-    const [players, setPlayers] useState([]);
+    const [players, setPlayers] = useState([]);
 
     useEffect(() => {
         setPlayers([])
