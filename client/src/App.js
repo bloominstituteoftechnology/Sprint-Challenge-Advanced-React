482491fd52543298afe0671414c4942c0ff87998
Andrew Nottoli <nottolivc@gmail.com>
2019-10-13 16:54:23 -0700

    the structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navigation from './Navigation.js';
+import PlayerList from './PlayerList.js';
 import './App.css';
+import uuid from 'react-uuid';
+import FixedMenuLayout from './FixedMenuLayout.js';
+import Block from './Block.js';
+import BarChart from './BarChart.js';
+//added uuid id generator, semantic-ui, react jest testing, and react-vis chart dependencies
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props) 
+    this.state = {
+    data: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:5000/api/players')
+    .then(result => result.json())
+    .then(players => this.setState({ data: players}))
+    .catch(Error => console.log(Error))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <FixedMenuLayout />
+
+        <Navigation />
+        <BarChart />
+
+        <div className="players">
+        {this.state.data.map(players => <PlayerList key={uuid()} players={players}/>)}
+        </div>
+        <Block />
+      </div>
+      
+    )
+  }
 }
+
+
+
 
 export default App;
