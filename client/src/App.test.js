1d10e007dd2d7d28694f99b4b42d094f3caf0d4a
Cole Gonzales <gcolegonzales003@gmail.com>
2019-10-14 19:17:37 -0500

    Merging

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as rtl from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import App from './App.js';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import * as rtl from '@testing-library/react';
+// import '@testing-library/jest-dom/extend-expect';
+// import App from './App.js';
 
-afterEach(rtl.cleanup);
+// afterEach(rtl.cleanup);
 
-it('renders without crashing', () => {
-  const div = docment.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// it('renders without crashing', () => {
+//   const div = docment.createElement('div');
+//   rtl.render(<App />, div);
+//   rtl.unmountComponentAtNode(div);
+// });
+
+// it('loads dark mode button', () => {
+//   const app = rtl.render(<App />);
+//   const element = app.getByTitle('dark-mode-btn');
+//   expect(element).toBeVisible();
+// });
+
+import React from 'react'
+import * as rtl from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import App from './App'
+
+afterEach(rtl.cleanup)
+
+test(`Renders without crashing, has class name of 'App'`, () => {
+  const wrapper = rtl.render(<App />)
+  const element = wrapper.getByTestId("app-test-id")
+  expect(element).toHaveClass('App')
 });
-
-it('loads dark mode button', () => {
-  const app = ReactDOM.render(<App />);
-  const element = app.getByTitle('dark-mode-btn');
-  expect(element).toBeVisible();
-});
-
-it('renders players', () => {
-  const wrapper = ReactDOM.render(<App />);
-  const players = app.getByTestId(/player/i);
-  expect(players).toBeVisible();
-})
