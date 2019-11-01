8d8471eda8077a052f78d9f0323707f0178fbc6f
Aasa-Christian <aasachristian@yahoo.com>
2019-11-01 18:41:32 -0400

    Completed MVP

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,11 +4,12 @@ import App from './App';
 import { render} from '@testing-library/react'
 
 
-test('renders without crashing', () => {
-  const container = render(<App/>);
-});
+// test('renders without crashing', () => {
+//   const container = render(<App/>);
+// });
 
 test('display the text Red or white', () => {
   const container = render(<App/>);
  container.getByText("Red or White??");
 });
+
