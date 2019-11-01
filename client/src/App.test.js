c11c2781c1bd6929ae9839b4137c4b75649753c1
Aasa-Christian <aasachristian@yahoo.com>
2019-11-01 15:05:26 -0400

    complted component and hook

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'jes-dom/extend-expect';
+import {getByLabelText,
+  getByText,
+  getByTestId,
+  queryByTestId} from '@testing-library/jest-dom/extend-expect'
+import PlayerCard from './components/PlayerCard';
+import expectExport from 'expect';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+
+
+
+test('it should be red when true', () => {
+  const div = rtl.render(
+    <App/>
+  );
+ const element = div.getByText(/Red/i);
+ expect(element).toBeInTheDocument();
 });
