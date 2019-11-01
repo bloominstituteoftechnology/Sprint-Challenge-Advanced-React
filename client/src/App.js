e6116aeb302ad0f458e6b4bc960a3c39b190ef1d
Sara De La Cruz <saradelacruz@Saras-MacBook-Air.local>
2019-11-01 11:12:25 -0500

    axios call succesful, created hooks file

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+class App extends React.Component {
+  state = {
+    players: []
+  }
+
+  componentDidMount () {
+    axios.get('http://localhost:5000/api/players')
+    .then(res => {
+      console.log("data",res)
+      this.setState ({
+        players: res.data
+      })
+    
+      })
+      .catch (err =>{
+        console.log("Data not received", err)
+    })
+  }
+  render () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <p>placeholder</p>
     </div>
   );
+  }
 }
 
 export default App;
