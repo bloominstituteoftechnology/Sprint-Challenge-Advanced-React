e9b0d83ab5b62b253e55913c32e5522d3878b613
CodyBrewer <brewer.cody.m@gmail.com>
2019-11-01 09:57:10 -0600

    Players.js     [x] getting player data from server    [x] setting Player data to state

--- client/src/components/Players/Players.js
+++ client/src/components/Players/Players.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Axios from 'axios';
 
-export default class Players extends Component {
+export default class Players extends React.Component {
   // create state to hold playerData
   state = {
     playerData: []
   };
   // get player data from server
-  componentDidMount()
-  // set player data to state
+  componentDidMount() {
+    Axios.get('http://localhost:5000/api/players')
+      .then(res => {
+        // set player data to state
+        this.setState({ playerData: res.data });
+        console.log(`playerData, ${this.state.playerData}`);
+      })
+      .catch(err => console.log(`${err}`));
+  }
 
   //pass down player data to Player component and Render Player component
   render() {
