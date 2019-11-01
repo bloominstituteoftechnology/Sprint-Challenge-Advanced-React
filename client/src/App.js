23c9dae33653d29e45bc02e5c24affb75e578b91
Crystal Martin <drcrystalm@gmail.com>
2019-11-01 12:58:45 -0500

    componentDidMount

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import ReactDOM from "react-dom"
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = {
+        players: [],
+    }
+    componentDidMount() {
+        axios
+            .get(`http://localhost:5000/api/players`)
+            .then(res => {
+                console.log(res.data)
+                this.setState({
+                    players: res.data,
+                })
+            })
+            .catch(err => console.log(err))
+    }
+
+    render() {
+        return (
+            <>
+                <div className='App'>
+                    <h1>All the Players</h1>
+                </div>
+                <div className='players'></div>
+            </>
+        )
+    }
 }
 
-export default App;
+export default App
