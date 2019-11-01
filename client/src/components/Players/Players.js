145d2a46d5051e1b1bb02db64299bbe26ab53bcd
CodyBrewer <brewer.cody.m@gmail.com>
2019-11-01 09:44:13 -0600

    state created

--- client/src/components/Players/Players.js
+++ client/src/components/Players/Players.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 
 export default class Players extends Component {
   // create state to hold playerData
-
+  state = {
+    playerData: []
+  };
   // get player data from server
-
+  componentDidMount()
   // set player data to state
 
   //pass down player data to Player component and Render Player component
