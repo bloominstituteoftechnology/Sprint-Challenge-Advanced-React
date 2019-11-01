c4933412c4b02454cac8b6b694c02d6f337d0316
CodyBrewer <brewer.cody.m@gmail.com>
2019-11-01 09:36:54 -0600

    Test:     App: App         Should render Players component with out crashing     Status: Failing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import Players from './components/Players';
 
 describe('App', () => {
   test('should render without crashing', () => {
     render(<App />);
+  });
+  test('Should render Players component with out crashing', () => {
+    render(<Players />);
   });
 });
