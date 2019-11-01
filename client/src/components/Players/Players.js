e4eac9822c7151256e466ab3b282b33621298a1d
CodyBrewer <brewer.cody.m@gmail.com>
2019-11-01 09:43:09 -0600

    comments to lay out what we need to do with players component

--- client/src/components/Players/Players.js
+++ client/src/components/Players/Players.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-  return <div></div>;
-};
+export default class Players extends Component {
+  // create state to hold playerData
+
+  // get player data from server
+
+  // set player data to state
+
+  //pass down player data to Player component and Render Player component
+  render() {
+    return <div></div>;
+  }
+}
