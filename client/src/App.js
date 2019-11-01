40f7dacc8c427a43771880503eb57fff3d031b64
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-11-01 12:11:51 -0400

    class component w/API data displaying

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    APICall: []
+  };
+  
+  componentDidMount() {
+
+    axios 
+    .get(`http://localhost:5000/api/players`)
+    .then(res => {
+      this.setState({
+        APICall: res
+      });
+      console.log("API data", this.state.APICall)
+    })
+    .catch(err => console.log(err));
+  }
+  
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to my Sprint Challenge App!</h1>
+      </div>
+    );
+  }
 }
-
 export default App;
