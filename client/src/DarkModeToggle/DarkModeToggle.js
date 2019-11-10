3e183d0f31397cd2f85916702126b1aee240a698
Veto Ramirez <vetorobert3@gmail.com>
2019-11-10 10:27:35 -0800

    pushing to submit for daily reports

--- client/src/DarkModeToggle/DarkModeToggle.js
+++ client/src/DarkModeToggle/DarkModeToggle.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
 
 const DarkModeToggle = () => {
-  // const [darkMode, setDarkMode] = useState(false);
-  // const toggleMode = e => {
-  //   e.preventDefault();
-  //   setDarkMode(!darkMode);
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  }
   
   return (
     <div>
-      <p>Toggle to Dark Mode</p>
-      <button>Toggle Here!</button>
+      <h3>Toggle to Dark Mode</h3>
+      <button 
+        onClick = {toggleMode}
+        className = {darkMode ? 'toggle toggled' : 'toggle'}
+      >Toggle Here!
+      </button>
     </div>
     // <nav className="navbar">
     //   <h1>Toggle to Dark Mode</h1>
