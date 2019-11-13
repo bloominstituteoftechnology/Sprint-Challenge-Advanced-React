c014cfec1bfad3fe4edba9fb35f6bd06fec7226b
Veto Ramirez <vetorobert3@gmail.com>
2019-11-12 20:58:58 -0800

    trying to get the playercard to render player info in the app.js file.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import PlayerCard from './components/PlayerCard.js';
-import useDarkModeToggle from './DarkModeToggle/useDarkModeToggle.js'
+import useDarkMode from './DarkModeToggle/useDarkModeToggle.js'
 
 import './App.css';
 
+const App = () => {
+  const [darkMode, setDarkMode] = useDarkMode(false);
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  };
 
-function App() {
   return (
-    <div className="App">
-      <useDarkModeToggle />
-      <h1>Women's World Cup Players</h1>
+    <nav className="navbar">
+      <h1>Dark Mode Toggle</h1>
+      <div className="dark-mode__toggle">
+        <button
+          onClick={toggleMode}
+          className={darkMode ? 'toggle toggled' : 'toggle'}>Toggle Here
+        </button>
+      </div>
+    </nav>
+
+    <div>
       <PlayerCard />
     </div>
-  );
-}
+  )
+};
 
 export default App;
