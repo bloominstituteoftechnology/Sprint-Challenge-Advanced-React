fe62ebf0c581c2de710df9ad0b55751d90d1e75c
Kayla Dailey <kayladailey000@gmail.com>
2019-11-13 13:17:00 -0600

    added snapshot testing

--- client/src/tests/button.test.js
+++ client/src/tests/button.test.js
@@ -1,7 +1,22 @@
 import React from 'react';
-import { render } from "@testing-library/react";
+import { render, cleanup,toHaveTextContent} from "@testing-library/react";
 import Button from '../Components/Button';
+import renderer from 'react-test-renderer';
 
+
+afterEach(cleanup)
 it ('renders button correctly',() => {
     const{getByTestId} = render(<Button label='Dark Mode'/>);
     expect (getByTestId("button"))})
+
+
+// it ('renders button correctly',() => {
+//     const{getByTestId} = render(<Button label='save'/>);
+//     expect (getByTestId("button")).toHaveTextContent('save');
+// })
+
+
+it('matches snapshot', ()=> {
+   const tree = renderer.create(<Button label = 'save'></Button>).toJSON();
+   expect(tree).toMatchSnapshot();
+})
