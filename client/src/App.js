c4e73e54e0de1b53331d3f34384c7ee6e904814f
shgradyy <shantaygrady@gmail.com>
2019-11-18 14:30:41 -0500

    Converts to class

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Header from './components/Header';
+import {Button, Divider, Input, Segment} from 'semantic-ui-react';
+import axios from 'axios';
 
 
-function App() {
-  return (
-    <div className="App">
-        <Header />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
-
+import { Header } from "semantic-ui-react";
 export default function Head() {
     return(
         <div>
-            <Header as='h1'>
-                Welcome to Women's World Cup Player Rankings
-            </Header>
+            <Header basic textAlign='center' size="huge">Welcome to Women's World Cup Player Rankings</Header>
         </div>
     )
 }
