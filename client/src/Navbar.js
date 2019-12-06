15c83b8b3f07eac557711a89c90a40f9b00751d9
weng7533 <weng7533@users.noreply.github.com>
2019-12-06 14:24:05 -0500

    Additional Style

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
     };
     return (
         <nav className="navbar">
-            <h1>Crypto Tracker</h1>
+            <h1>Players</h1>
             <div className="dark-mode__toggle">
                 <div
                     onClick={toggleMode}
