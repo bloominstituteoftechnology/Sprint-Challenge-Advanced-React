bbdee36d723b83b4ad83db098d7c87e4118e5a51
wchamber01 <williamchambers@gmail.com>
2019-12-06 16:59:44 -0500

    final push

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
+import Players from "./components/Players";
+import Navbar from "./components/Navbar";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("App renders without crashing", () => {
+  const component = render(<App />);
+  //console.log(component);
+});
+
+test("Players renders without crashing", () => {
+  const component = render(<Players />);
+});
+
+test("Navbar renders without crashing", () => {
+  const component = render(<Navbar />);
+});
+
+test("Is there a toggle?", () => {
+  const { findAllByDisplayValue } = render(<App />);
+
+  findAllByDisplayValue(/toggle/i);
+});
+
+test("text on screen", () => {
+  const { getByText } = render(<App />);
+  expect(getByText(/Number of Searches:/i));
 });
