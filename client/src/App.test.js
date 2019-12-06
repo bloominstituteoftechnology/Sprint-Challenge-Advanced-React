b78a04f18a9a8344ac4aa4b098e4e2e008a590bf
Dijah Deen <dijahdeen@Dijahs-MacBook-Air.local>
2019-12-06 14:17:53 -0500

    test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,6 +3,14 @@ import {render} from '@testing-library/react';
 import App from './App';
 import Navbar from './components/nav';
 
-it('renders without crashing', () => {
+
+
+test('renders without crashing', () => {
   render(<App />);
+});
+
+test('renders players text without crashing', () => {
+  const Navtext=render(<Navbar />);
+
+  Navtext.getByText('Players');
 });
