f49d46c7b81019f152157c361ce246b3b2478434
wchamber01 <williamchambers@gmail.com>
2019-12-06 11:57:48 -0500

    added players.js and the axios call

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
+import Players from "./Players";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    players: []
+  };
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/players").then(response => {
+      console.log(response.data, "players data");
+      this.setState({ players: response.data });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>2019 Womens World Cup Players</h1>
+        <h2>ordered by search popularity</h2>
+        <Players players={this.state.players} />
+      </div>
+    );
+  }
 }
 
 export default App;
