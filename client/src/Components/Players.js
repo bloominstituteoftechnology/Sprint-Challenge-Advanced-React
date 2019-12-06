501c98f716db222d2fdfa072b5a5618960a3098f
jarrodskahill <jarrodskahill@gmail.com>
2019-12-06 09:01:16 -0800

    added some styling

--- client/src/Components/Players.js
+++ client/src/Components/Players.js
@@ -1,5 +1,21 @@
 import React from 'react';
 import axios from 'axios';
+import styled from "styled-components"
+
+const Form = styled.div`
+display: flex;
+justify-content: space-evenly;
+margin: 2%;
+flex-flow: wrap;
+`
+
+const Card = styled.div`
+dispaly:flex;
+flex-direction: column;
+border: black 2px solid;
+border-radius: 10px;
+margin: 1%;
+padding: 1%;`
 
 
 
@@ -24,13 +40,13 @@ import axios from 'axios';
     }
     render(){
         return (
-            <div>
-                {this.state.players.map(player => <li key={player.id}>
+            <Form>
+                {this.state.players.map(player => <Card key={player.id}>
                     <h2>Name: {player.name}</h2>
                     <h2>Country: {player.country}</h2>
                     <h2>Searches: {player.searches}</h2>
-                </li>)}
-            </div>
+                </Card>)}
+            </Form>
         )
     }
 }
