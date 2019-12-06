507aed149d2372abb5273a0c675ba661c614e743
weng7533 <weng7533@users.noreply.github.com>
2019-12-06 11:34:45 -0500

    components setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      playerData: {}
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:5000/api/players`)
+      .then(res => {
+        console.log(res.data);
+        this.setState({ playerData: res.data });
+      })
+
+  }
+
+
+
+  render() {
+    console.log(this.state.playerData[0]);
+    return <div>{this.state.message}</div>;
+  }
 }
-
 export default App;
