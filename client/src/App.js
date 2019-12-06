cfd9ae4c085eb0c9725b42eade626ea1121c20d0
perezsergioace <perez.sergio.ace@gmail.com>
2019-12-06 11:42:58 -0500

    Used axios to get playeyr's data. Data is now being rendered to the DOM.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
+import Players from './components/Players';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Players</h1>
+      <Players />
     </div>
   );
 }
--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -1,13 +1,48 @@
 import React from 'react';
+import axios from 'axios';
 
 class Players extends React.Component {
-    render() {
-        return(
-            <div>
-                <h1>Players.js</h1>
-            </div>
-        )
-    }
+	constructor() {
+		super();
+
+		this.state = {
+			players: []
+		};
+	}
+
+	componentDidMount() {
+		console.log('CDM is running for players');
+		// fetch("http://localhost:5000/api/players")
+		// .then(response => response.json())
+		// .then(response => console.log(response))
+
+		axios
+			.get('http://localhost:5000/api/players')
+			.then((response) => {
+				console.log(response.data);
+				this.setState({
+					players: response.data
+				});
+			})
+			.catch((error) => {
+				console.log('Data was not return', error);
+			});
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Players.js</h1>
+				{this.state.players.map((player) => (
+					<div key={player.id}>
+						<h2>Name: {player.name}</h2>
+						<p>Country: {player.country}</p>
+						<p>Searches: {player.searches}</p>
+					</div>
+				))}
+			</div>
+		);
+	}
 }
 
 export default Players;
