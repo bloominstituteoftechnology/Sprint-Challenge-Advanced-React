5e359ab501e531773777bd8d002681934e191fe1
jarrodskahill <jarrodskahill@gmail.com>
2019-12-06 08:51:46 -0800

    created a dark mode using a custom hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Player } from './Components/Players'
+import Navbar from './Components/NavBar'
+import "./styles/styles.scss"
 
 
 function App () {
   return (
     <div>
+      <Navbar/>
       <Player/>
     </div>
   )
---
+++ client/src/Components/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import useDarkMode from "./darkmode"
+
+const Navbar = () => {
+  const [darkMode, setDarkMode] = useDarkMode(false);
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  };
+  return (
+    <nav className="navbar">
+      <h1>Female World Championship</h1>
+      <div className="dark-mode__toggle">
+        <div
+          onClick={toggleMode}
+          className={darkMode ? 'toggle toggled' : 'toggle'}
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
