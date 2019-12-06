6a92e7e112d23e1c5330c4c561be5dce93bf097d
Aasa-Christian <aasachristian@yahoo.com>
2019-12-06 14:00:01 -0500

    completed mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-
+import logo from './logo.svg';
 import './App.css';
-import Players from './components/Players';
+import Players from './components/Players'
 
 function App() {
-
-  
   return (
     <div className="App">
-
-      <div>
-        <Players/>
-      </div>
+      <Players/>
     </div>
   );
 }
--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import axios from 'axios';
-
+import PLayerCard from './PlayerCard'
 
 class Players extends React.Component{
     constructor(){
-        super();
-            this.state = {
-            players: [],
-        } 
-    }
+
+       super();
+    this.state = {
+        players: [],
+    } 
+}
 
     componentDidMount(){
         axios.get('http://localhost:5000/api/players')
@@ -20,20 +21,13 @@ class Players extends React.Component{
 
         })
         .catch(err => console.log(err));
-
-
     }    
 
     render(){
         return(
             <div className= "cardCont">
-                {this.state.players.map(player => (
-                    <div>
-                    <h1>{player.name}</h1>
-                    <p>{player.country}</p>
-                    <p>{player.searches}</p>
-                    </div>
-                    
+                {this.state.players.map(player => (                    
+                    <PLayerCard name ={player.name} id ={player.id}  country = {player.country} searches= {player.searches}  />
                 ))}
             </div>
         )
