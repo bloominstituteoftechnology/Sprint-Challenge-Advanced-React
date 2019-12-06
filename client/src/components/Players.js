d40893d24f4e55c6642866c6acae4afe682e3d40
perezsergioace <perez.sergio.ace@gmail.com>
2019-12-06 11:15:48 -0500

    Initial Commit

---
+++ client/src/components/Players.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Players extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Players.js</h1>
+            </div>
+        )
+    }
+}
+
+export default Players;
