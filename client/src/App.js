afe06bf91e8d72b0a3a45cc5b4057dd31e95ea5c
wchamber01 <williamchambers@gmail.com>
2019-12-06 13:18:24 -0500

    added darkMode and localStorage hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import axios from "axios";
 import "./App.css";
-import Players from "./Players";
+import Players from "./components/Players";
+import Navbar from "./components/Navbar";
 
 class App extends React.Component {
   state = {
@@ -12,12 +13,13 @@ class App extends React.Component {
     axios.get("http://localhost:5000/api/players").then(response => {
       console.log(response.data, "players data");
       this.setState({ players: response.data });
-    });
+    }, []);
   }
 
   render() {
     return (
       <div className="App">
+        <Navbar />
         <h1>2019 Womens World Cup Players</h1>
         <h2>ordered by search popularity</h2>
         <Players players={this.state.players} />
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import useDarkMode from "../hooks/useDarkMode";
+
+const Navbar = (key, initialValue) => {
+  const [darkMode, setDarkMode] = useDarkMode(key, initialValue);
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  };
+  return (
+    <nav className="navbar">
+      <div className="dark-mode__toggle">
+        <div
+          onClick={toggleMode}
+          className={darkMode ? "toggle toggled" : "toggle"}
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
