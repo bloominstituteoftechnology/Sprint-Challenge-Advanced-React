64f4bed4747863de07dce8a2856acbe1ae6138eb
littleonetwo <littleonetwo@gmail.com>
2019-12-13 20:42:05 -0600

    done

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme';
 import App from './App.js';
 import PlayerList, { players } from './components/playerList.js';
 
-
+// configure({ adapter: new Adapter() });
 afterEach(rtl.cleanup);
 
 
@@ -14,16 +17,35 @@ test('renders without crashing', () => {
 
 });
 
-describe('Do test to see if api calls were successful', () =>{
-  it('test just random array parameter see if its null', () =>{
-    const testNum = 4;
+test('renders playerList to DOM', () => {
+  const wrapper = rtl.render(<App />);
 
+  const element = wrapper.queryByText(/list of players/i);
 
-    expect(players[testNum]).toBe(!undefined); //as long as this isnt null it should be fine;
-  });
+  expect(element).toBeInTheDocument;
 
+})
 
+test('renders titlebar to DOM', () => {
+  const wrapper = rtl.render(<App />);
 
+  const element = wrapper.queryByText(/sprint/i);
 
+  expect(element).toBeInTheDocument;
 
-});
+})
+// describe('Do test to see if api calls were successful', () =>{
+//   it('test just random array parameter see if its null', () =>{
+//     const wrapper = shallow(<App />);
+//     const instance = wrapper.instance();
+//     const testNum = 4;
+//     // const componentInstance = wrapper.instance();
+//
+//     instance.componentDidMount();
+//
+//     // const players = wrapper.props.state.players[testNum];
+//
+//     expect(instance.state().players[testNum]).toEqual('Carli Lloyd'); //as long as this isnt null it should be fine;
+//   });
+//
+// });
