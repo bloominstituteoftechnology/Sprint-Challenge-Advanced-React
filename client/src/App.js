e50d97647c5700eb6b5243a268c4e31654a59392
Trey McGarity <trey.mcgarity99@gmail.com>
2019-12-14 11:57:33 -0600

    getting and placing data into initialized array in class state

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { 
+      players: [], 
+    }
+  }
+
+  
+  componentDidMount = () => {
+    this.getPlayers();
+  }
+  
+  getPlayers = () => {
+    axios.get('http://localhost:5000/api/players')
+    .then(response => {
+      //console.log(response.data);
+      this.setState({ players: response.data})
+    })
+    .catch(error => {
+      console.log('somthing went wrong ', error);
+    })
+  }
+  
+  render() {
+    //console.log('in render: ',this.state.players);
+    return (
+      <div className="App">
+        <h1>Women's World Cup Players</h1>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
