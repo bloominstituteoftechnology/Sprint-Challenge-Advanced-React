cb4317c44e71ba471d53dc580fb20d23d270500a
Michael Lowe <mrlowememphis@yahoo.com>
2019-12-15 18:50:02 -0600

    PlayersCard added

---
+++ client/src/components/PlayersCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PlayersCard = props => {
+    return(
+        <div className="card">
+            <h2>{props.name}</h2>
+            <p>{`is from ${props.country} and as been searched ${props.searches} times.`}</p>
+        </div>
+    );
+}
+
+export default PlayersCard; 
