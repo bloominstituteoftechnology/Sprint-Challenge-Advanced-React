33b1dae5dc3d0f0b0833ea9707ccc80dacff435f
Timothy <timgill987@gmail.com>
2020-01-17 16:36:19 -0500

    fixing my tester

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import App from "./App";
+import '@testing-library/jest-dom/extend-expect';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("inverts the backround and text colors", () => {
+  const { getByTestId } = render(<App />);
+  let dmButton = getByTestId("dark-mode-button");
+  fireEvent.click(dmButton);
+  expect(darkmode).toBeTruthy();
 });
