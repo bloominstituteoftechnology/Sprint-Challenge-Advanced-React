8b143ae03a9a2a3e09b5da91973090e5a573c083
wesley.moody <wgm3190@gmail.com>
2020-01-17 13:10:19 -0600

    code was good when I started from scratch don't know what the problem was but works now

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component }from 'react';
+import axios from 'axios';
 import './App.css';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (){
+    super()
+    this.state = {
+      Players: []
+
+
+    }
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:5000/api/players').then(response =>{
+      console.log(response.data)
+
+      this.setState ({
+
+        Players:response.data
+      })
+    })
+  }
+
+  render(){
+    return(
+      <div className='Container'>
+        <Navbar/>
+
+        {this.state.Players.map(player => {
+          return(
+            <div className='playercard'>
+
+              <p>{player.name}</p>
+              <p>{player.country}</p>
+              <p>{player.searches}</p>
+              </div>
+          )
+        })}
+      </div>
+    )
+      }
 }
 
 export default App;
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import Navbar from './components/Navbar';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// it('renders without crashing', () => {
+
+
+test('renders without crashing', () => {
+  render(<App />);
 });
+
