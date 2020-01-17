b183dafef14f6a6d1414a9504eb30c092110d1ab
Timothy <timgill987@gmail.com>
2020-01-17 17:02:44 -0500

    sprint complete, tests passed

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import App from "./App";
-import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent, cleanup } from "@testing-library/react";
+
+
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
@@ -10,9 +11,9 @@ it("renders without crashing", () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it("inverts the backround and text colors", () => {
+test("inverts the backround and text colors", () => {
   const { getByTestId } = render(<App />);
   let dmButton = getByTestId("dark-mode-button");
   fireEvent.click(dmButton);
-  expect(darkmode).toBeTruthy();
+  expect(dmButton).toBeTruthy();
 });
