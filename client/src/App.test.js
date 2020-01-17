46ac913c6c020d233c1c143f5a8d12513d7643d8
Aaron Andersen <anderseana@tutanota.com>
2020-01-17 11:56:54 -0600

    MVP is met!

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { findAllByText } from '@testing-library/react';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('is it rendering', () => {
+  render(<App/>)
+})
+
+test('render player name', () => {
+  findByText(/Christen Press/i)
+  
+})
+
