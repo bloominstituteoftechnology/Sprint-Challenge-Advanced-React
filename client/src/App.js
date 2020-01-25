2981e6f214196cbf925206b4d134cface99d163f
Veto Ramirez <vetorobert3@gmail.com>
2020-01-25 10:09:25 -0800

    Deleted files to start from scratch

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import PlayerCard from './components/PlayerCard.js';
-import NavBar from './components/NavBar'
+
 
 import './App.css';
 
 const App = () => {
   return (
     <div>
-      <NavBar />
-      <PlayerCard />
+
     </div>
   )
 }
