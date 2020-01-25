f6ac3fecadef348893c57da4c85653a582c6ec58
Veto Ramirez <vetorobert3@gmail.com>
2020-01-25 10:15:18 -0800

    Creating a PlayerCard component.

---
+++ client/src/Components/PlayerCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PlayerCard extends React.Component {
+  constructor() {
+    super();
+    this.state: {};
+  }
+  render() {
+    return null;
+  }
+}
+
+export default PlayerCard;
