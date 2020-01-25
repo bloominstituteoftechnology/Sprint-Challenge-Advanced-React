d07ed8a1b2e9acb19b1217f5c61e018bd65a4e20
Veto Ramirez <vetorobert3@gmail.com>
2020-01-25 13:34:07 -0800

    Created a useNameInStorage hook.

--- client/src/Hooks/useNameInStorage.js
+++ client/src/Hooks/useNameInStorage.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const useNameInStorage = () => {
+import useLocalStorage from './Hooks/useLocalStorage';
+import e from 'express';
 
+const useNameInStorage = () => {
+  const [userName, setUserName] = useLocalStorage('userName', '');
+
+  return (
+    <div>
+      <input 
+        type='text'
+        placeholder='Enter name here!'
+        value='name'
+        onChange={e => setUserName(e.target.value)}
+      />
+    </div>
+  )
 }
 
 export default useNameInStorage;
