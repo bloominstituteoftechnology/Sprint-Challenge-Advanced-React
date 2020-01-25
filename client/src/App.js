7ba5293d2836537dab8c89ec2630b346173743f3
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2020-01-25 09:21:32 -0800

    Created the class component and fetched data from the API

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       hello there
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      players :[]
+    };
+  }  
+  componentDidMount(){
+    fetch("http://localhost:5000/api/players")
+    .then(res => res.json())
+    .then (res => {
+      console.log("response from the API", res)
+      this.setState({...this.state, players:res})
+    })
+    .catch(err => console.log(err));
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+        hello there
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
