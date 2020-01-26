e7b129b5d76cde315bf17066f5968d378a66daa5
Shyamasri Biswas (Shya) <54689969+shyabiswas@users.noreply.github.com>
2020-01-26 17:26:15 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,55 @@
-import React from 'react';
-import './App.css';
-import "./FetchData"
+import React from "react"
+import Header from "./Components/Header";
+import PlayerList from "./Components/PlayerList";
+import SearchForm from "./Components/SearchForm";
+import "./App.css";
+import Axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <FetchData/>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      name: "",
+      country: ""
+    };
+  }
+
+  componentDidMount() {
+    console.log("Component has Mounted.");
+
+    Axios.get("http://localhost:5000/api/players")
+      .then(response => {
+        this.setState({
+          data: response.data,
+          name: response.data.name,
+          country: response.data.country
+        });
+      })
+      .catch(error => console.log("Missing the players", error));
+  }
+  render() {
+    
+    return (
+      <div className="App">
+        <Header />
+        <SearchForm
+          data={this.state.data}
+          name={this.state.name}
+          country={this.state.country}
+          name={this.players}
+          search={this.search}
+        />
+        <PlayerList
+          data={this.state.data}
+          name={this.state.name}
+          country={this.state.country}
+        />
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
