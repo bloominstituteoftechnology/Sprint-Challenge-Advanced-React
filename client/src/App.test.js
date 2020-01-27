81434ef68a5bc8a40d22eaac3da618864f962a59
bret-knight87 <bret.knight87@gmail.com>
2020-01-26 22:44:57 -0600

    added tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import * as rtl from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import NavBar from './components/NavBar';
+
+test('NavBar renders without crashing', () => {
+  const { getByText } = render(<NavBar />)
+  getByText("Soccer Cards")
+})
+
+test('clicking the toggle button actually changes class to dark mode', () => {
+  const { getByTestId } = rtl.render(<NavBar />)
+  const darkToggleButton = getByTestId('darkToggleButton')
+  expect(darkToggleButton.classList.contains('toggled')).toBe(false)
+  fireEvent.click(darkToggleButton)
+  expect(darkToggleButton.classList.contains('toggled')).toBe(true)
+})
