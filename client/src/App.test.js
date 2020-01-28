89ccbdd2d6b1666b6eacb3b7cb7ee9b86b65a665
Anthony Streett <arjstreett@gmail.com>
2020-01-28 14:54:57 -0500

    Added dependencies and some tests ++ extra hover buttton feature

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import PlayerList from './components/PlayerList';
+import Header from './components/Header';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+afterEach(rtl.cleanup);
+
+//testing
+it('renders correctly. truthy, in document, visible', () => {
+  const wrapper = rtl.render(
+    <PlayerList />
+  );
+  const element = wrapper.queryByText(/players/i);
+  expect(element).toBeTruthy();
+  expect(element).toBeInTheDocument();
+  expect(element).toBeVisible();
+
+//comment
+  wrapper.debug();
+});
+
+it('renders correctly. truthy, in document, visible', () => {
+  const wrapper = rtl.render(
+    <Header />
+  );
+  const element = wrapper.queryByText(/Toggle Theme/i);
+  expect(element).toBeTruthy();
+  expect(element).toBeInTheDocument();
+  expect(element).toBeVisible();
+  
+//comment
+  wrapper.debug();
 });
