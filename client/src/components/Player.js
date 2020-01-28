0c36b49cf2241e20f5555d521211d2fdce53b00b
Aeson Johnson <aeson.m.johnson@gmail.com>
2020-01-28 14:42:03 -0800

    updated hooks

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -2,8 +2,8 @@ import React from "react";
 import axios from "axios";
 import PlayerCard from "./PlayerCard";
 export default class Player extends React.Component{
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
           players: []
         }
