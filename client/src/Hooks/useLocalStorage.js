12323386064f55eea71ef05c4296425e809ca120
Shyamasri Biswas (Shya) <54689969+shyabiswas@users.noreply.github.com>
2020-01-27 23:35:59 -0500

    Update useLocalStorage.js

--- client/src/Hooks/useLocalStorage.js
+++ client/src/Hooks/useLocalStorage.js
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import {useState}  from 'react';
 
-export const useLocalStorage = (key, initialValue) => {
-  const [storedValue, setStoredValue] = useState(() => {
-    const item = window.localStorage.getItem(key);
-    return item ? JSON.parse(item) : initialValue;
-  });
+export function useLocalStorage(key,initialValue){
+    const[storedValue, setStoredValue]=useState(()=>{
+        const item=window.localStorage.getItem(key)
+    return item ? JSON.parse(item) :initialValue;
+    });
 
-  const setValue = value => {
-    setStoredValue(value);
-    window.localStorage.setItem(key, JSON.stringify(value));
-  };
+    const setValue= value=>{
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value))
+    }
 
-  return [storedValue, setValue];
-};
+    return [storedValue, setValue];
+}
