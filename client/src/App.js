49d907e6f133667af5b5db0d1979578a6a3d9741
LittleBoGeek <NadiaChilds@outlook.com>
2020-01-28 16:34:48 -0500

    wip, app is running

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
 import logo from './logo.svg';
-import './App.css';
 import axios from 'axios';
+import './App.css';
 
 class App extends React.Component {
-constructor() {
-  super();
-  const [state, setState] = useState();
-const useLocalStorage = (key, initialValue) => {
-  const [storedValue, setStoredValue] = useState(() => {
-    const item = window.localStorage.getItem(key);
-    return item ? JSON.parse(item) : initialValue;
-  }); 
-
-
-}
-compondentDidMount(){
-axios.get('http://localhost:5000/api/players')
-.then(res => this.setState({res.players}))
-.catch( err  => console.log(err))
+  constructor() {
+    super();
+    this.state = "";
 
   }
- }
- render () {
-  return (
-    <div>
+  componentDidMount(){
+    fetch('http://localhost:5000/api/players')
+    .then( data =>  this.setState({data}));
        
-        {this.state.res.data.map(player => <div>{this.state.player}</div>)}
-    </div>
-  );
+  } 
+  render() {
+    return(
+      <div className="App">
+<h1> App is running </h1>
+
+
+
+      </div>
+    )
+  }
 }
+//{//newData.map((props,index )=> <h1 key={props.name + "-" + index}>Name: {props.name} </h1>)} 
 
-
-export default App;
-
-
-// Class component that fetches data from the server you now have running - the data can be fetched from http://localhost:5000/api/players check 
-//  Display the player data you receive from the API in progress 
-//  Build a custom hook and use it in your app - this can be a localStorage hook, a fetch hook, a dark mode hook, or any other hook you would like
-//  Write a "reasonable" amount of unit tests for your React components. What does "reasonable amount" mean? That is up to you to decide. Be ready to defend your choice to your TL
-// filled in read.me 
+export default App 
