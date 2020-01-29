36a899f1abb42ec1d54fb6676ee8c5a8c854110e
Aeson Johnson <aeson.m.johnson@gmail.com>
2020-01-29 15:45:14 -0800

    2 tests working

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as rtl from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
+import Player from "./components/PlayerCard";
+import Navbar from "./components/Navbar";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+afterEach(rtl.cleanup);
+
+describe('App renders without crashing', () => {
+  test("Does App render?", () => {
+    rtl.render(<App />)
+  })
+})
+
+it('renders Player', () => {
+  const div=document.createElement('div');
+  ReactDOM.render(<Player />, div);
   ReactDOM.unmountComponentAtNode(div);
-});
+})
