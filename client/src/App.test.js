7a78cd3fa66719e6bdf4e9205c687dbfb09fec8d
Veto Ramirez <vetorobert3@gmail.com>
2020-01-29 20:29:33 -0800

    Can't figure out the problem with running the test code.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import * as rtl from '@testing-library/react';
+import * as rtl from 'react-testing-library';
 import 'jest-dom/extend-expect';
-
 import App from './App';
 
-it('renders without crashing', () => {
-
+it("renders an h1 containing Womans World Cup", () => {
+  const wrapper = rtl.render(
+    <App />
+  );
+  const element = wrapper.queryByText(/world cup/i);
+  expect(element).toBe('World Cup');
 });
+
+describe('App', () => {
+  it('mounts to render in a sim DOM', () => {
+    const simulatedDOM = rtl.render(<App />);
+    console.log(simulatedDOM.debug());
+  })
+})
