67940a1461dc0cf15e3ab44b03703351de880d5e
Mandi Haase <haase1020@gmail.com>
2020-02-14 12:08:09 -0500

    add class component function App in App.js file with axios call

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Component } from React;
+import axios from 'axios';
+import PlayerCard from './component/PlayerCard';
+import Nav from './components/Nav';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state ={
+      players:[]
+    };
+  }
+
+  componentDidMount() {
+    axios.get('   ')
+    .then(res => {
+      console.log('players', res.data);
+      this.setState({players: res.data})
+    })
+    .catch(err => {
+      console.log("There is an error", err)
+    })
+
+  }
+  render () {
+    return (
+      <div className="container">
+        <Nav />
+        {this.state.players.map(player => (
+          <PlayerCard
+          key={player.id}
+          name={player.name}
+          rank={player.rank}
+          team={player.team}
+          />
+        ))
+        };
+      </div>
+    );
+  }
 }
 
 export default App;
