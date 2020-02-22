03367825fb0fa58e45aecf380d72f7ed5c78b356
remoo1901 <remoo1901@hotmail.com>
2020-02-22 12:41:13 -0500

    working on components files

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component() {
+  constructor() {
+    super(
+      (this.state = {
+        data: []
+      })
+    );
+  }
+
+  
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then(response => {
+        this.setState({ data: response.data });
+      })
+      .catch(err => {
+        console.log("ERROR", err);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>App</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
