76537f992536c40895112d4b417dc41216e1e396
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-02-22 12:32:17 -0500

    App

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import DMHeader from "./components/DMHeader";
+import SearchForm from "./components/SearchForm";
+import PlayerList from "./components/PlayerList";
+import Axios from "axios;"
+
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      name: "",
+      country: ""
+    };
+  }
+
+componentDidMount() {
+  console.log("Mount Works!");
+
+  Axios
+  .get("https://localhost:5000/api/players")
+  .then(res => {
+    console.log(res.data);
+    this.setState({
+      data: res.data,
+      name: res.data.name,
+      country: res.data.country
+    });
+  })
+  .catch(error => console.log("Players Benched!"));
+}
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DMHeader />
+      <SearchForm />
+      <PlayerList />
     </div>
+
   );
+}
 }
 
 export default App;
