4cfaab727f33cbc9c9856f6920027cf82468fe87
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-02-23 10:32:31 -0500

    PlayerList

---
+++ client/src/components/PlayerList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const PlayerList =({name, coutry}) => {
+    return (
+        <div>
+            <h3>Name: {name}</h3>
+            <h4>Country: {country}</h4>
+        </div>
+    )
+}
