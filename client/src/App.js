3acf9e7e64dca21087e00edc63ca6cf9ba9e7995
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-02-23 09:58:52 -0500

    yarn+axios+app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    players:[]
+  }
+  componentDidMount() {
+    axios
+    .get('http://localhost:5000/api/players')
+    .then(res => {
+      console.log("data", res)
+      this.setState({players: res.data})
+    })
+    .catch (err => {console.log("No data", err)
+  })
+  }
+  render(){
+    return (
+      <div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
