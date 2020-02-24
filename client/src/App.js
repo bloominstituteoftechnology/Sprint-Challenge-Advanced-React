aa389f6440e77a7f715d5d5a7a19ca401f26f6b9
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-02-23 21:02:16 -0500

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
 import axios from 'axios';
 import Navigation from './components/Navigation';
 import PlayerList from './components/PlayerList';
-=======
-import DMHeader from "./components/DMHeader";
-import SearchForm from "./components/SearchForm";
-import PlayerList from "./components/PlayerList";
-import axios from "axios";
->>>>>>> 0c5cbbd7c134c64ba074c7ca833b658cf31b2cc6
 
 import './App.css';
 
 class App extends React.Component {
   constructor() {
-<<<<<<< HEAD
     super ();
     this.state ={
     players:[],
@@ -22,7 +14,7 @@ class App extends React.Component {
 }
   componentDidMount() {
     axios
-    .get('http://localhost:5000/api/players')
+    .get('https://localhost:5000/api/players')
     .then(res => {
       console.log("data", res)
       this.setState({players: res.data})
@@ -41,46 +33,7 @@ class App extends React.Component {
       </div>
     );
   }
-=======
-    super();
-    this.state = {
-      data: [],
-      name: "",
-      country: ""
-    };
-  }
-
-componentDidMount() {
-  console.log("Mount Works!");
-
-  axios
-  .get("https://localhost:5000/api/players")
-  .then(res => {
-    console.log(res.data);
-    this.setState({
-      data: res.data,
-      name: res.data.name,
-      country: res.data.country
-    });
-  })
-  .catch(error => console.log("Players Benched!"));
 }
 
-  render() {
-  return (
-    <div className="App">
-      <DMHeader />
-
-      <PlayerList
-      data={this.state.data}
-      name={this.state.name}
-      country={this.state.country}
-       />
-    </div>
-
-  );
->>>>>>> 0c5cbbd7c134c64ba074c7ca833b658cf31b2cc6
-}
-}
 
 export default App;
--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {useState} from 'react';
 
 const useLocalStorage = (key, initialValue) => {
@@ -9,24 +8,8 @@ const useLocalStorage = (key, initialValue) => {
     const setValue = value => {
         setStoredValue(value);
         localStorage.setItem(key, JSON.stringify(value));
-=======
-import { useState } from 'react'
-
-const useLocalStorage = (key, initialValue) => {
-    const [storedValue, setStoredValue] = useState(() => {
-    const item = window.localStorage.getItem(key);
-    return item ? JSON.parse(item) : initialValue;
-    });
-    const setValue = value => {
-        setStoredValue(value);
-        window.localStorage.setItem(key, JSON.stringify(value));
->>>>>>> 0c5cbbd7c134c64ba074c7ca833b658cf31b2cc6
     };
     return [storedValue, setValue];
 }
 
-<<<<<<< HEAD
 export default useLocalStorage
-=======
-export default useLocalStorage;
->>>>>>> 0c5cbbd7c134c64ba074c7ca833b658cf31b2cc6
