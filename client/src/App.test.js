756253f8f897e28ca6e1ad5d900fb61b08e67343
Michael Lowe <mrlowememphis@yahoo.com>
2020-02-23 20:52:15 -0600

    MVP complete, 1 test working, questions answered

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,14 +1,56 @@
 import React from 'react';
-import { render, within, getByText } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react'
+import App from './App'
 
-import PlayersCard from './PlayersCard.js';
-import PlayersList from './PlayersList.js';
+import '@testing-library/jest-dom';
 
-test('should have text [player name]',  async ()=>{
-    const {findByText} = await render(<PlayersCard />)
-    findByText(/alex morgan/i);
-}) 
+test('App renders without crashing', () => {
+  render(<App />);
+});
+
+test('Header shows up', () => {
+  const {getByText} = render(<App />);
+ 
+   const header = getByText(/Player searches/);
+ 
+   expect(header).toBeInTheDocument()
+ 
+ }) 
 
 
 
+// test('Name column title rendered', () => {
 
+//     const { getByText } = render(<App/>);
+  
+//     getByText(/name/i);
+  
+//   });
+
+  // test('Header', () => {
+  //   const { getByText } = render(<App />);
+  //   const header = getByText(/Top Womens World Cup Players/i);
+  //   // expect(header).toBeInTheDocument();
+  // })
+
+// it('renders without crashing', () => {render(<App/>)
+
+// });
+
+
+
+// test('Name column title rendered', () => {
+
+//     const { getByText } = render(<App/>);
+  
+//     getByText(/name/i);
+  
+//   });
+
+// test('should have text [player name]',  async ()=>{
+//     const {findByText} = await render(<PlayersCard />)
+//     findByText(/alex morgan/i);
+// }) 
+
+// 
