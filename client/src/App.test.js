07b511a1951af07ddf2f91a9edcd5a2220166213
Regbmiller <40448656+Regbmiller@users.noreply.github.com>
2020-02-24 06:07:45 -0600

    added tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from "@testing-library/react";
 import App from './App';
 
 it('renders without crashing', () => {
@@ -7,3 +8,11 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test("Players names are rendered to the screen", () => {
+  const { getByText } = render(<App />);
+
+  getByText(/players/i);
+});
+
+
