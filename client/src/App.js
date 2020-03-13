10a31a712a1fd72fcc87dd53ff585a9345fe8393
Kenji Greene <kenjigrr@gmail.com>
2020-03-13 13:46:41 -0400

    added navbar, player call, dark mode and storage hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Player } from './components/Players'
+import Navbar from './components/Navbar'
+import "./styles.scss";
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Player/>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import useDarkMode from '../hooks/useDarkMode'
+
+const Navbar = () => {
+  const [darkMode, setDarkMode] = useDarkMode();
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(darkMode !== 'dark' ? 'dark':'standard');
+  };
+  return (
+    <nav className="navbar">
+      <h1>Players</h1>
+      <div className="dark-mode__toggle">
+        <div
+          onClick={toggleMode}
+          className={darkMode === 'dark' ? 'toggle toggled' : 'toggle'}
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
