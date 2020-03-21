bc5cc265012b9a0140e9bb106e568e50152853a6
Aeson Johnson <aeson.m.johnson@gmail.com>
2020-03-20 19:21:51 -0700

    mvp met i think??

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as rtl from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
+import Player from "./components/PlayerCard";
+import Navbar from "./components/Navbar";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+afterEach(rtl.cleanup);
+
+describe('App renders without crashing', () => {
+  test("Does App render?", () => {
+    rtl.render(<App />)
+  })
+});
+
+it('Renders Player without failure', () => {
+  const div=document.createElement('div');
+  ReactDOM.render(<Player />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test("Renders Women's World Cup Players", () => {
+  const box = rtl.render(<App />);
+  expect(box.getByText(/women's world cup players/i));
 });
---
+++ client/src/components/Player.js
@@ -0,0 +1,40 @@
+import React from "react";
+import axios from "axios";
+import PlayerCard from "./PlayerCard";
+
+export default class Player extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+          players: []
+        }
+      }
+    
+    componentDidMount(){
+        axios.get("http://localhost:5000/api/players")
+        .then(response => {
+          this.setState({
+            players: response.data
+          })
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    }
+
+    render() {
+        return (
+          <div>
+            {this.state.players.map(player =>
+              <PlayerCard
+                name={player.name}
+                country={player.country}
+                searches={player.searches}
+                // html_url={player.html_url}
+                // login={player.login}
+              />
+            )}
+          </div>
+        )
+    }
+}
