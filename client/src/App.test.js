bcd5fe89ddeee0d26009f42974f5a46f56c09d80
thomas <t_huynh927@yahoo.com>
2020-03-20 20:15:54 -0700

    finished mvp

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { render, waitForElement } from '@testing-library/react';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { useDarkMode } from "../hooks/useDarkMode";
+
+const Header = () => {
+    const [ darkMode, setDarkMode ] = useDarkMode(false)
+    return (
+    <div style={{ background: 'lightgray', width: "100%", height: 50 }}>
+        <button onClick={() => setDarkMode(!darkMode)}>Toggle Dark Mode</button>
+    </div>
+    )
+}
+
+export default Header;
