826df5de10b2fde0844c25f29421efad6804147c
Pedro Revilla <peterevilla@gmail.com>
2020-03-20 22:46:26 -0500

    mvp

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as rt1 from '@testing-library/react'
+import { render } from '@testing-library/react';
 import App from './App';
+import NavBar from './components/NavBar';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+
+test('renders without crashing', () => {
+  rt1.render(<App />);
+});
+
+
+
+// test("displays toggle function", () => {
+//   const { getByText } = render(<App />);
+//   getByTestId("toggle");
+// });
+
+test("renders nav", async () => {
+  // Arrange
+  const { getByText } = render(<NavBar />);
+  // Act
+  const darkM = getByText(/Women's World Cup Players Trend/i);
+  // Assert
+  expect(darkM).toBeInTheDocument();
+});
+
