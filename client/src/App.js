c250d94af4fb1a1dfb90fec06c13b2675fda1fd2
Gustavo Yepez <gustavo@Gustavos-MacBook-Pro.local>
2020-04-07 23:40:25 -0400

    sprint

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Players from './players';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Players</h2>
+      <div>
+        <Players/>
+      </div>
     </div>
   );
 }
