dd9aea1f177a7e0156724517f06400181d1e6197
Andrew Sitzes <andrewsitzes@Andrews-MacBook-Pro.local>
2020-04-10 11:47:07 -0700

    restarted :|

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+      super();
+      this.state = {
+          users: [],
+          followers: []
+      };
+  };
+
+componentDidMount() {
+  axios.get("http://localhost:5000/api/players").then(response => {
+    this.setState({
+      players: response.data,
+    });
+  });
 }
+
+render() {
+  return(
+      <div>
+        <h1>hello</h1>
+      </div>
+     
+    );
+  }
+};
 
 export default App;
