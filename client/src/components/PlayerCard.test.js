db01c178bc8b2927bab08d02ca397e14b7e19aa3
soni653 <dave.family653@outlook.com>
2020-04-17 21:58:06 -0400

    update PlayerCard.js

--- client/src/components/PlayerCard.test.js
+++ client/src/components/PlayerCard.test.js
@@ -5,12 +5,12 @@ import { render, fireEvent, waitForElement, queryHelpers } from "@testing-librar
 
 test("if PlayerCard renders props", async () => {
     const props = {
-        name: 'thomas',
+        name: 'prapti',
         country: 'USA'
     }
     const container = render(<PlayerCard {...props} />)
-    const name = container.getByText('thomas')
+    const name = container.getByText('prapti')
     const country = container.getByText('USA')
-    expect(name.innerHTML).toBe('thomas')
+    expect(name.innerHTML).toBe('prapti')
     expect(country.innerHTML).toBe('USA')
 })
