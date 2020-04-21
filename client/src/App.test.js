e086bef1a7d2e206f6479c8425d1538dbf74f22b
Ariel_Rodriguez <32606708+Ariels713@users.noreply.github.com>
2020-04-20 22:28:04 -0400

    added testing, having trouble getting results, npm test is not running

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as rtl from 'react-testing-library'
+import 'jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
+import PlayerSearchForm from './components/PlayerSearchForm'
+import FilteredSearchResults from './components/FilteredSearchResults'
+import PlayerCardContent from './components/PlayerCardContent';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-// const sum = (num1, num2) => {
-//   return num1 + num2
-// }
-// console.log(sum)
+
+test('renders search form', () => {
+  render(<PlayerCardContent/>);
+})
+
+test('renders filtered list ', () => {
+  render(<FilteredSearchResults/>);
+})
+
+test('renders filtered list ', () => {
+  render(<PlayerSearchForm/>);
+})
+
+test('renders placeholder text'), () => {
+  const {getByPlaceHolderText} = render(<PlayerSearchForm/>)
+  
+}
+
+
